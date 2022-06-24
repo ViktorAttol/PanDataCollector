53fa29783b9a44aafd59786f7f51125f188bca90
ViktorAttol <Jonas.Kolesnyk@Student.HTW-Berlin.de>
2022-06-24 11:59:07 +0200

    Initial commit

---
+++ PanDataCollector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PanDataCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
